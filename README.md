f62bcf7c1985471fb85c8966c6952557a546c07c
gmgalvan <guillermoo.gs@gmail.com>
2020-11-01 13:30:46 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Quantum teleportation example
+
+QASM code:
+
+`
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[5];
+creg c[5];
+
+//Step 1: create entagled pair
+h q[2];
+cx q[2], q[4];
+barrier q[0],q[1],q[2],q[3],q[4];
+
+//Step 2: preprare a payload
+x q[0];
+h q[0];
+t q[0];
+barrier q[0],q[1],q[2],q[3],q[4];
+
+//Step : send
+h q[0];
+h q[2];
+cx q[2],q[0];
+h q[2];
+measure q[0] -> c[0];
+measure q[2] -> c[2];
+barrier q[3],q[4];
+
+// step 4: receive
+x q[4];
+z q[4];
+barrier q[3], q[4];
+
+// step 5: verify
+tdg q[4];
+h q[4];
+x q[4];
+measure q[4] -> c[4];
+`
+
+Circuit from IBM quantum computing
+
+![circuit](circuit-kgzgym5v.png)
+
+
+Result Histogram
+
+![histogram](histogram.png)
---
+++ teleport.py
@@ -0,0 +1,28 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(5, 'q')
+creg_c = ClassicalRegister(5, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[2])
+circuit.cx(qreg_q[2], qreg_q[4])
+circuit.barrier(qreg_q[0], qreg_q[1], qreg_q[2], qreg_q[3], qreg_q[4])
+circuit.x(qreg_q[0])
+circuit.h(qreg_q[0])
+circuit.t(qreg_q[0])
+circuit.barrier(qreg_q[0], qreg_q[1], qreg_q[2], qreg_q[3], qreg_q[4])
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[2])
+circuit.cx(qreg_q[2], qreg_q[0])
+circuit.h(qreg_q[2])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[2], creg_c[2])
+circuit.barrier(qreg_q[3], qreg_q[4])
+circuit.x(qreg_q[4])
+circuit.z(qreg_q[4])
+circuit.barrier(qreg_q[3], qreg_q[4])
+circuit.tdg(qreg_q[4])
+circuit.h(qreg_q[4])
+circuit.x(qreg_q[4])
+circuit.measure(qreg_q[4], creg_c[4])
